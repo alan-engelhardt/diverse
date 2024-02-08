a591b75a04c074ffa778c58a6183c4fab1a422e8
Alan <ale@kea.dk>
2024-02-08 22:59:33 +0100

    split

--- dark.js
+++ dark.js
@@ -4,10 +4,16 @@ let chkbox;
 function tjekDarkMode() {
   chkbox = document.querySelector("#light-mode");
   chkbox.addEventListener("change", setMode);
-  document.cookie == "dark" ? (chkbox.checked = false) : (chkbox.checked = true);
-  console.log(document.cookie);
+
+  const cookieValue = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("lightMode="))
+    ?.split("=")[1];
+
+  cookieValue == "off" ? (chkbox.checked = false) : (chkbox.checked = true);
+  console.log(cookieValue);
 }
 
 function setMode() {
-  chkbox.checked ? (document.cookie = "light") : (document.cookie = "dark");
+  chkbox.checked ? (document.cookie = "lightMode=on") : (document.cookie = "lightMode=off");
 }
